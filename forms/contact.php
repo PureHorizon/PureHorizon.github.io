43e71ababf534168e3265d96c04bb4a5c4fcb933
PureHorizon <96225455+PureHorizon@users.noreply.github.com>
2023-01-06 23:51:52 +0100

    change_contact

--- forms/contact.php
+++ forms/contact.php
@@ -1,17 +1,48 @@
 <?php
 
-if (isset($_POST['submit'])) {
-  $name = $_POST['name'];
-  $subject = $_POST['subject'];
-  $mailFrom = $_POST['mail'];
-  $message = $_POST['message'];
+// Empfange die Formulardaten über die POST-Methode
+$name = $_POST['name'];
+$email = $_POST['email'];
+$subject = $_POST['subject'];
+$message = $_POST['message'];
+$recaptcha = $_POST['recaptcha-response'];
 
-  $mailTo = "contact@example.com";
-  $headers = "From: " . $mailFrom;
-  $txt = "You have received an e-mail from " . $name . ".\n\n" . $message;
+// Überprüfe, ob alle benötigten Felder ausgefüllt wurden
+if (empty($name) || empty($email) || empty($subject) || empty($message)) {
+  echo "Bitte fülle alle benötigten Felder aus.";
+  exit;
+}
 
-  mail($mailTo, $subject, $txt, $headers);
-  header("Location: index.php?mailsend");
+// Überprüfe, ob die eingegebene E-Mail-Adresse gültig ist
+if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+  echo "Bitte gib eine gültige E-Mail-Adresse ein.";
+  exit;
+}
+
+// Überprüfe, ob das reCaptcha korrekt ausgefüllt wurde
+$secret = 'DEINE_SECRET_KEY';
+$response = file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret={$secret}&response={$recaptcha}");
+$response = json_decode($response, true);
+
+if ($response['success'] === false) {
+  echo "Bitte bestätige, dass du kein Roboter bist.";
+  exit;
+}
+
+// Setze den Betreff der E-Mail
+$subject = "[Kontaktanfrage] {$subject}";
+
+// Setze den Inhalt der E-Mail
+$body = "Von: {$name}\nE-Mail: {$email}\nNachricht:\n{$message}";
+
+// Setze den Absender der E-Mail
+$headers = "From: {$name} <{$email}>";
+
+// Sende die E-Mail
+if (mail('contact@example.com', $subject, $body, $headers)) {
+  echo 'OK';
+} else {
+  echo "Beim Versenden der E-Mail ist ein Fehler aufgetreten.";
 }
 
 ?>
